93e464f21bfd131fa058e671f842d435cd9da8c9
TRSalaS19 <gruiz0803@icloud.com>
2020-07-02 09:26:17 -0700

    made a changed to correct python code

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # This is my Git Project
 
 > My code for the Git Project
-> My code for the Git Project
-> My code for the Git Project
 
 ## I'm in the feature branch
 
--- my_file.py
+++ my_file.py
@@ -1,10 +1,8 @@
 print('hi Git')
-print('hi Git')
-print('hi Git')
 
 print('Hi agian, from another Dev')
 
-def hi_there()  {
+def hi_there() {
     print("Whats UP!!")
 }
 
